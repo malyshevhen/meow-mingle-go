b413fe6e0d6f02ae7b2e8d2c8dccb85eac0e52d9
Evhen <malysh.evgeniy@gmail.com>
2024-04-19 20:04:38 +0300

    fix: add text field constraints

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
 	id         BIGSERIAL  	NOT NULL,
-	email      VARCHAR(255)	NOT NULL UNIQUE,
-	first_name VARCHAR(255)	NOT NULL,
-	last_name  VARCHAR(255)	NOT NULL,
-	password   VARCHAR(255)	NOT NULL,
+	email      VARCHAR(255)	NOT NULL CHECK (length(email) > 0) UNIQUE,
+	first_name VARCHAR(255)	NOT NULL CHECK (length(first_name) > 0),
+	last_name  VARCHAR(255)	NOT NULL CHECK (length(last_name) > 0),
+	password   VARCHAR(255)	NOT NULL CHECK (length(password) > 0),
 	created_at TIMESTAMPTZ 	NOT NULL DEFAULT NOW(),
 
   PRIMARY KEY (id)
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS posts (
 	id         BIGSERIAL	  NOT NULL,
-	content    VARCHAR(255) NOT NULL,
+	content    VARCHAR(255) NOT NULL CHECK (length(content) > 0),
 	author_id  BIGINT			  NOT NULL,
 	created_at TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
 	updated_at TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS posts (
 
 CREATE TABLE IF NOT EXISTS comments (
 	id        	BIGSERIAL			NOT NULL,
-	content   	VARCHAR(255)  NOT NULL,
+	content   	VARCHAR(255)  NOT NULL CHECK (length(content) > 0),
 	author_id 	BIGINT			  NOT NULL,
 	post_id   	BIGINT			  NOT NULL,
 	created_at	TIMESTAMPTZ   NOT NULL DEFAULT NOW(),
