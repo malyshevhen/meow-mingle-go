2b28a85deef48a3d9bad32ad203c37d151654ee0
Evhen Malysh <malysh.evgeniy@gmail.com>
2024-05-27 17:36:38 +0300

    refactor: extract constants

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,15 @@ import (
 	"os"
 )
 
+const (
+	SERVER_PORT_ENV_KEY string = "SERVER_PORT"
+	DB_URL_ENV_KEY      string = "DB_SOURCE"
+	JWT_EVN_KEY         string = "JWT_SECRET"
+	DEFAULT_SERVER_PORT string = ":8080"
+	DEFAULT_DB_URL      string = "postgresql://postgres:example@localhost:5432/mingle_db?sslmode=disable"
+	DEFAULT_JWT_KEY     string = "13bb62a3f8a44d0523918228c3ea7643547495c7ba74c893f9546d6de37ad996"
+)
+
 type Config struct {
 	ServerPort string
 	DBSource   string
@@ -12,15 +21,9 @@ type Config struct {
 
 func InitConfig() Config {
 	return Config{
-		ServerPort: getEnv("SERVER_PORT", ":8080"),
-		DBSource: getEnv(
-			"DB_SOURCE",
-			"postgresql://postgres:example@localhost:5432/mingle_db?sslmode=disable",
-		),
-		JWTSecret: getEnv(
-			"JWT_SECRET",
-			"13bb62a3f8a44d0523918228c3ea7643547495c7ba74c893f9546d6de37ad996",
-		),
+		ServerPort: getEnv(SERVER_PORT_ENV_KEY, DEFAULT_SERVER_PORT),
+		DBSource:   getEnv(DB_URL_ENV_KEY, DEFAULT_DB_URL),
+		JWTSecret:  getEnv(JWT_EVN_KEY, DEFAULT_JWT_KEY),
 	}
 }
 
