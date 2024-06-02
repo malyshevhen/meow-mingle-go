4dd06c8a829a55d587404fed85bfd74eb7d2a001
Evhen Malysh <malysh.evgeniy@gmail.com>
2024-06-02 11:19:17 +0300

    test: refactor `created` using parametrized tests

--- internal/handlers/comment_handlers_test.go
+++ internal/handlers/comment_handlers_test.go
@@ -20,120 +20,116 @@ import (
 type Comments []db.CommentInfo
 
 func TestHandleCreateComment(t *testing.T) {
-	store := &mock.MockStore{}
-
-	validParams := db.CreateCommentParams{
-		PostID:  1,
-		Content: "Test Comment",
+	type input struct {
+		comment db.CreateCommentParams
+		error   errors.Error
 	}
 
-	invalidParams := db.CreateCommentParams{
-		PostID:  0,
-		Content: "",
+	type want struct {
+		status  int
+		comment db.Comment
 	}
 
-	comment := db.Comment{
-		ID:       1,
-		AuthorID: 1,
-		Content:  "Test Comment",
+	testCases := []struct {
+		name  string
+		input input
+		want  want
+	}{
+		{
+			name: "happy path",
+			input: input{
+				comment: db.CreateCommentParams{
+					PostID:  1,
+					Content: "Test Comment",
+				},
+			},
+			want: want{
+				status: http.StatusCreated,
+				comment: db.Comment{
+					ID:       1,
+					AuthorID: 1,
+					Content:  "Test Comment",
+				},
+			},
+		},
+		{
+			name: "returns 400 if invalid post ID",
+			input: input{
+				comment: db.CreateCommentParams{
+					Content:  "",
+					AuthorID: 0,
+					PostID:   0,
+				},
+			},
+			want: want{
+				status:  http.StatusBadRequest,
+				comment: db.Comment{},
+			},
+		},
+		{
+			name: "returns 500 if store returns an error",
+			input: input{
+				comment: db.CreateCommentParams{
+					PostID:  1,
+					Content: "Test Comment",
+				},
+				error: errors.NewDatabaseError(fmt.Errorf("%s", "db error")),
+			},
+			want: want{
+				status:  http.StatusInternalServerError,
+				comment: db.Comment{},
+			},
+		},
 	}
-	store.SetComment(comment)
 
-	mux := mux.NewRouter()
-	mux.HandleFunc("/posts/{id}/comments",
-		middleware.MiddlewareChain(
-			HandleCreateComment(store),
-			middleware.LoggerMW,
-			middleware.ErrorHandler,
-			fakeAuth(1),
-		),
-	).Methods("POST")
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			store := &mock.MockStore{}
+			store.SetError(tc.input.error)
+			store.SetComment(tc.want.comment)
 
-	server := httptest.NewServer(mux)
-	defer server.Close()
+			mux := mux.NewRouter()
+			mux.HandleFunc("/posts/{id}/comments",
+				middleware.MiddlewareChain(
+					HandleCreateComment(store),
+					middleware.LoggerMW,
+					middleware.ErrorHandler,
+					fakeAuth(1),
+				),
+			).Methods("POST")
+			server := httptest.NewServer(mux)
+			defer server.Close()
 
-	t.Run("returns 201 and created comment if valid", func(t *testing.T) {
-		req, err := http.NewRequest(
-			"POST",
-			fmt.Sprintf("%s/posts/1/comments", server.URL),
-			reqBodyOf(validParams),
-		)
-		assert.NoError(t, err)
-
-		resp, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err, "perform the request")
-		defer resp.Body.Close()
-
-		body, err := io.ReadAll(resp.Body)
-		assert.NoError(t, err, "read response body")
-
-		if status := resp.StatusCode; status != http.StatusCreated {
-			t.Errorf(
-				"handler returned wrong status code: got %v want %v",
-				status,
-				http.StatusCreated,
+			req, err := http.NewRequest(
+				"POST",
+				fmt.Sprintf("%s/posts/1/comments", server.URL),
+				reqBodyOf(tc.input.comment),
 			)
-		}
+			assert.NoError(t, err)
 
-		user, err := utils.Unmarshal[db.Comment](body)
+			resp, err := http.DefaultClient.Do(req)
+			assert.NoError(t, err, "perform the request")
+			defer resp.Body.Close()
 
-		assert.NoErrorf(t, err, "unmarshal response body")
-		assert.Truef(
-			t,
-			reflect.DeepEqual(user, comment),
-			"handler returned wrong body: got %v want %v",
-			user,
-			comment,
-		)
-		assert.Equal(t, http.StatusCreated, resp.StatusCode)
-	})
+			t.Run("check that request body is the same as expected", func(t *testing.T) {
+				body, err := io.ReadAll(resp.Body)
+				assert.NoError(t, err, "read response body")
+				createdComment, err := utils.Unmarshal[db.Comment](body)
+				assert.NoErrorf(t, err, "unmarshal response body")
+				assert.Truef(
+					t,
+					reflect.DeepEqual(createdComment, tc.want.comment),
+					"handler returned wrong body: got %v want %v",
+					createdComment,
+					tc.want.comment,
+				)
+			})
 
-	t.Run("returns 400 if invalid post ID", func(t *testing.T) {
-		req, err := http.NewRequest(
-			"POST",
-			fmt.Sprintf("%s/posts/invalid/comments", server.URL),
-			reqBodyOf(validParams),
-		)
-		assert.NoError(t, err)
-
-		resp, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err, "perform the request")
-		defer resp.Body.Close()
-
-		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-	})
-
-	t.Run("returns 400 if invalid content", func(t *testing.T) {
-		req, err := http.NewRequest(
-			"POST",
-			fmt.Sprintf("%s/posts/1/comments", server.URL),
-			reqBodyOf(invalidParams),
-		)
-		assert.NoError(t, err)
-
-		resp, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err, "perform the request")
-		defer resp.Body.Close()
-
-		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
-	})
-
-	t.Run("returns 500 if error creating comment", func(t *testing.T) {
-		store.SetError(errors.NewInternalServerError(fmt.Errorf("error creating comment")))
-
-		req, err := http.NewRequest(
-			"POST",
-			fmt.Sprintf("%s/posts/1/comments", server.URL),
-			reqBodyOf(validParams),
-		)
-		assert.NoError(t, err)
-
-		resp, err := http.DefaultClient.Do(req)
-		assert.NoError(t, err, "perform the request")
-		defer resp.Body.Close()
-
-		assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
-	})
+			t.Run("check that status is correct", func(t *testing.T) {
+				assert.Equal(t, tc.want.status, resp.StatusCode)
+			})
+		})
+	}
 }
 
 func TestHandleCreateCommentUnauthenticated(t *testing.T) {
